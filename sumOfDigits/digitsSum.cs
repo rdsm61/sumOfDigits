85b416099b0db90f3492641bea3088716721ab4d
rdsm61 <rdsameoff@gmail.com>
2025-11-11 18:00:15 +0400

    Добавьте файлы проекта.

---
+++ sumOfDigits/digitsSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumOfDigits
+{
+    class digitsSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any integer number");
+            long number = long.Parse(Console.ReadLine());
+            Console.Write("Sum of digits of the {0} ", number);
+
+            long sum = 0;
+            do
+            {
+                sum += number % 10;
+                number /= 10;
+
+            } while (number != 0);
+
+            if (sum < 0)
+                sum = -sum;
+
+            Console.WriteLine("is {0}", sum);
+        }
+    }
+}
