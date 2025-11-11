b9fa57b3312e5b684e18278ddfeecbd0a870bb81
rdsm61 <rdsameoff@gmail.com>
2025-11-12 01:28:18 +0400

    Добавлен второй вариант суммирования цифр в числе через преобразование символов в строке

--- sumOfDigits/digitsSum.cs
+++ sumOfDigits/digitsSum.cs
@@ -7,8 +7,9 @@ namespace sumOfDigits
         static void Main(string[] args)
         {
             Console.WriteLine("Enter any integer number");
-            long number = long.Parse(Console.ReadLine());
-            Console.Write("Sum of digits of the {0} ", number);
+            string numberStr = Console.ReadLine();
+            long number = long.Parse(numberStr);
+            Console.Write("Sum of digits of {0} ", number);
 
             long sum = 0;
             do
@@ -22,6 +23,14 @@ namespace sumOfDigits
                 sum = -sum;
 
             Console.WriteLine("is {0}", sum);
+
+            sum = 0;
+            for (int i = 0; i < numberStr.Length; i++)
+            {
+                sum += (long)numberStr[i] - 48;
+            }
+
+            Console.WriteLine("Sum of digits of {0} is {1}", numberStr, sum);
         }
     }
 }
